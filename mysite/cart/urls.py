29c224bb6eb0aaa9721a8230a316135974b53598
Артем Торгашов <andtorg55@yandex.ru>
2025-03-28 17:34:31 +0300

    Revert "com"
    This reverts commit e49811edd4acd5f9e199afd970a5ce3212e988d6

--- mysite/cart/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import BascketProductsView
-
-app_name = 'cart'
-
-urlpatterns = [
-    path('api/basket', BascketProductsView.as_view(), name='basket'),
-    path('api/cart', BascketProductsView.as_view(), name='cart'),
-]
--- mysite/orders/settings.py
+++
@@ -1,3 +0,0 @@
-deliveryType = ['Usual', 'Express']
-paymentType = ['Online Card', "Online from a random someone else's account"]
-status = ['Awaiting payment', 'Paid for', 'Payment error']
